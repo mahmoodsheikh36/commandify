835e8115da692dcade71d5e318ea5df334a7c902
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-04-13 20:15:19 +0300

    ignore KeyError exceptions when monitoring listening activity

--- commandify/monitor.py
+++ commandify/monitor.py
@@ -26,44 +26,50 @@ def start(client_id, client_secret, refresh_token):
         play = None
         prev_request_time = current_time()
         while True:
-            access_token = sp.get_valid_access_token(client_id, client_secret,
-                                                     refresh_token)
-            current_track = sp.get_current_track(access_token['token'])
-            time = current_time()
+            try:
+                access_token = sp.get_valid_access_token(client_id, client_secret,
+                                                         refresh_token)
+                current_track = sp.get_current_track(access_token['token'])
+                time = current_time()
 
-            if current_track is not None:
-                playing = current_track['is_playing']
-                song_id = current_track['item']['id']
-                progress = current_track['progress_ms'] / 1000.
-                if play is None:
-                    play = new_play(song_id)
-                else:
-                    if song_id != play['song_id']: # song changed
-                        sp.db_provider.update_play_time_ended(play['id'],
-                                                              time)
+                if current_track is not None:
+                    playing = current_track['is_playing']
+                    song_id = current_track['item']['id']
+                    progress = current_track['progress_ms'] / 1000.
+                    if play is None:
                         play = new_play(song_id)
-                    if not playing and play['playing']: # paused
-                        sp.db_provider.add_pause(play['id'], time)
-                        play['playing'] = False
-                    elif playing and not play['playing']: # resumed
-                        sp.db_provider.add_resume(play['id'], time)
-                        play['playing'] = True
-                    else: # if nothing just update time_ended to be safe..?
+                    else:
+                        if song_id != play['song_id']: # song changed
+                            sp.db_provider.update_play_time_ended(play['id'],
+                                                                  time)
+                            play = new_play(song_id)
+                        if not playing and play['playing']: # paused
+                            sp.db_provider.add_pause(play['id'], time)
+                            play['playing'] = False
+                        elif playing and not play['playing']: # resumed
+                            sp.db_provider.add_resume(play['id'], time)
+                            play['playing'] = True
+                        else: # if nothing just update time_ended to be safe..?
+                            sp.db_provider.update_play_time_ended(play['id'],
+                                                                  time)
+                        if play['progress'] != -1: # if not new play
+                            seconds_passed = (time - prev_request_time) / 1000
+                            error = abs(seconds_passed - abs(progress - play['progress']))
+                            if error > 5:
+                                sp.db_provider.add_seek(play['id'], time, progress)
+                        play['progress'] = progress
+                else: # spotify was closed / nothing is playing
+                    if play is not None:
                         sp.db_provider.update_play_time_ended(play['id'],
                                                               time)
-                    if play['progress'] != -1: # if not new play
-                        seconds_passed = (time - prev_request_time) / 1000
-                        error = abs(seconds_passed - abs(progress - play['progress']))
-                        if error > 5:
-                            sp.db_provider.add_seek(play['id'], time, progress)
-                    play['progress'] = progress
-            else: # spotify was closed / nothing is playing
-                if play is not None:
-                    sp.db_provider.update_play_time_ended(play['id'],
-                                                          time)
-                    play = None
+                        play = None
 
-            prev_request_time = time
+                prev_request_time = time
+            except KeyError as e:
+                """
+                sometimes spotify's api sends wrong data resulting in a KeyError
+                """
+                pass
             sleep(REQUEST_INTERVAL)
     except KeyboardInterrupt as e:
         print('quitting..')
